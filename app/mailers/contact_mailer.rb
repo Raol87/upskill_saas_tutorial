4c83eb4c666c81eb1b3e411f4039c0116ccd5e8b
Charles Marcu <charlesmarcu@gmail.com>
2018-03-12 18:03:39 +0000

    Added stripe and figaro gems and api keys

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'charliedevtesting@gmail.com'
+    default to: 'charliedevtester@gmail.com'
    
     def contact_email(name, email, body)
         @name = name
